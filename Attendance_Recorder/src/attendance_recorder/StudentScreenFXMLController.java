61a337630508392974b51198e54cb13523db55ac
Mathias <math231k@hotmail.com>
2020-02-04 11:03:20 +0100

    Oprettede resten af vores views

---
+++ Attendance_Recorder/src/attendance_recorder/StudentScreenFXMLController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendance_recorder;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author math2
+ */
+public class StudentScreenFXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
