0d3406a7cc48499cd0e2c3301bc46ed9cfd33615
rizvan99 <rizvan2312@hotmail.com>
2020-02-07 09:18:14 +0100

    3 lags arkitektur

--- Attendance_Recorder/src/attendance_recorder/gui/controllers/TeacherScreenFXMLController.java
+++ Attendance_Recorder/src/attendance_recorder/gui/controllers/TeacherScreenFXMLController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package attendance_recorder;
+package attendance_recorder.gui.controllers;
 
 import attendance_recorder.be.Student;
 import attendance_recorder.bll.MockStudentManager;
