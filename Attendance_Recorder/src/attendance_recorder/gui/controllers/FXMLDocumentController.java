497f5ea61e726cfb87d196592568efd514883427
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2020-02-07 10:41:46 +0100

    various

--- Attendance_Recorder/src/attendance_recorder/gui/controllers/FXMLDocumentController.java
+++ Attendance_Recorder/src/attendance_recorder/gui/controllers/FXMLDocumentController.java
@@ -7,16 +7,21 @@ package attendance_recorder.gui.controllers;
 
 import attendance_recorder.be.Student;
 import attendance_recorder.bll.MockStudentManager;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 /**
@@ -37,51 +42,38 @@ public class FXMLDocumentController implements Initializable {
         
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        msm = new MockStudentManager();
     }    
     
     @FXML
     private void handleLogin(ActionEvent event)
     {
-        if (validateName() && validatePassword())
+        if (validate())
         {
             openStudentScreen();
         }
         else errorAlert("Enter correct name and password");
     }
     
-    private boolean validateName()
+    private boolean validate()
     {
         List<Student> students = msm.getStudents();
         String name = txtName.getText().trim();
+        String password = txtPassword.getText().trim();
                 
         for (Student student : students)
         {
-            if (student.getProfileName().equals(name))
+            if (!name.isEmpty() && !password.isEmpty()) 
             {
-                return true;
-            }
+                if (student.getProfileName().equals(name) && student.getPassword().equals(password)) {
+                    return true;
+                    }                
+            }                    
         }
         
         return false;        
-    }
-    
-    private boolean validatePassword()
-    {
-        List<Student> students = msm.getStudents();
-        String password = txtPassword.getText().trim();
+    }   
         
-        for (Student student : students)
-        {
-            if (student.getProfileName().equals(password))
-            {
-                return true;
-            }
-        }
-        
-        return false; 
-    }
-    
     private void errorAlert(String message)
     {
         Alert alert = new Alert(Alert.AlertType.ERROR);        
@@ -93,6 +85,24 @@ public class FXMLDocumentController implements Initializable {
     
     private void openStudentScreen()
     {
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/attendance_recorder/gui/views/StudentScreenFXML.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage primStage = (Stage) btnLogin.getScene().getWindow();
+            Stage stage = new Stage();                       
+            
+            stage.setTitle("Student Overview");
+            stage.setScene(scene);
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(primStage);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            
         
     }
             
