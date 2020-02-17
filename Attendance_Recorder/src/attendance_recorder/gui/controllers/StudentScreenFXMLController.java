249a16e2247876e3681886005482b9ff8c357cf1
math231k <54638811+math231k@users.noreply.github.com>
2020-02-17 22:30:35 +0100

    et piechart til students

--- Attendance_Recorder/src/attendance_recorder/gui/controllers/StudentScreenFXMLController.java
+++ Attendance_Recorder/src/attendance_recorder/gui/controllers/StudentScreenFXMLController.java
@@ -9,10 +9,16 @@ import attendance_recorder.be.Student;
 import attendance_recorder.bll.MockStudentManager;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 /**
  * FXML Controller class
@@ -25,11 +31,13 @@ public class StudentScreenFXMLController implements Initializable {
     private Student currentUser;
     
     @FXML
-    private MenuItem handleClose;
-    @FXML
-    private MenuItem handlePieChart;   
+    private MenuItem handleClose; 
     @FXML
     private Label lblWelcome;
+    @FXML
+    private AnchorPane studentPane;
+    @FXML
+    private BorderPane diagramPane;
 
 
     /**
@@ -44,6 +52,39 @@ public class StudentScreenFXMLController implements Initializable {
     {
         currentUser = student;
         lblWelcome.setText("Good Morning " + student.getFirstName() + " " + student.getLastName());
+    }
+
+    @FXML
+    private void handlePieChart(ActionEvent event) {
+    
+        ObservableList<PieChart.Data> pieData = FXCollections.observableArrayList(
+                new PieChart.Data("Mondays", 3),
+                new PieChart.Data("Tuesdays", 6),
+                new PieChart.Data("Wednesdays", 2),
+                new PieChart.Data("Thursday", 5),
+                new PieChart.Data("Fridays", 1)
+                );
+        
+        PieChart pieChart = new PieChart(pieData);
+        pieChart.setTitle("Student Absence");
+        pieChart.setClockwise(true);
+        pieChart.setLabelLineLength(50);
+        pieChart.setLabelsVisible(true);
+        pieChart.setStartAngle(180);
+        
+        studentPane.setVisible(false);
+        
+        diagramPane.setCenter(pieChart);
+        
+    
+    }
+
+    @FXML
+    private void handleMainView(ActionEvent event) {
+       
+        studentPane.setVisible(true);
+        diagramPane.setCenter(studentPane);
+        
     }
     
 }
